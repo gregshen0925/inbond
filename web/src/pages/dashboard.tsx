8db46628b306d66edee5a5d768e1e48bd462521d
GregShen <gregshen0925@gmail.com>
2023-01-15 20:29:04 +0800

    add

---
+++ web/src/pages/dashboard.tsx
@@ -0,0 +1,22 @@
+import type { NextPageWithLayout } from '@/types';
+import { NextSeo } from 'next-seo';
+import ModernScreen from '@/components/screens/modern-screen';
+import RootLayout from '@/layouts/_root-layout';
+
+const DashboardPage: NextPageWithLayout = () => {
+  return (
+    <>
+      <NextSeo
+        title="Dashboard"
+        description="Dashboard"
+      />
+      <ModernScreen />
+    </>
+  );
+};
+
+DashboardPage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export default DashboardPage;
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,22 @@
+import type { GetStaticProps, InferGetStaticPropsType } from 'next';
+import type { NextPageWithLayout } from '@/types';
+import RootLayout from '@/layouts/_root-layout';
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+  };
+};
+
+const HomePage: NextPageWithLayout<
+  InferGetStaticPropsType<typeof getStaticProps>
+> = () => {
+  return (<div>projects</div>)
+  ;
+};
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export default HomePage;
