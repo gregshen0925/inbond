aff9039db78305e1265b5953aeb7a1daac1aae2b
justaliang <justaliang@protonmail.com>
2023-02-01 13:52:31 +0900

    fix bug: get bond info

--- web/src/components/ui/bond-card.tsx
+++ web/src/components/ui/bond-card.tsx
@@ -4,6 +4,9 @@ import Avatar from '@/components/ui/avatar';
 import type { BondData, KV } from '@/types/typing';
 import { useQuery } from '@tanstack/react-query';
 import { getBond } from '@/lib/utils/getBond';
+import { useEffect, useState } from 'react';
+import { client, TREASURY_MODULE_ID } from "@/lib/utils/aptosClient"
+
 
 type Props = {
   project: KV;
@@ -14,12 +17,30 @@ export default function BondGrid({ project }: Props) {
 
   // const BondData = (bondQuery?.data as BondData) || null;
 
+  const [bondData, setBondData] = useState<BondData>({
+    creator: "",
+    description: "",
+    external_url: "",
+    funding: { value: '0' },
+    image_url: "",
+    name: "",
+    target_funding_size: "",
+  });
+
   const { data: bondQuery } = useQuery({
     queryKey: ['getBondInfo'],
     queryFn: () => getBond(project.key, project.value),
   });
 
-  const BondData = (bondQuery?.data as BondData) || null;
+  // const BondData = (bondQuery?.data as BondData) || null;
+
+  useEffect(() => {
+    const getResource = async () => {
+      setBondData((await client.getAccountResource(project.key, TREASURY_MODULE_ID + "Treasury<0x1::aptos_coin::AptosCoin>")).data as BondData)
+    }
+    getResource()
+  }, [project])
+
 
   return (
     <a href={`/bond-details/${project.key}&${project.value}`}>
@@ -27,19 +48,19 @@ export default function BondGrid({ project }: Props) {
         <div className="p-4">
           <div className="flex items-center text-sm font-medium text-gray-600 transition hover:text-gray-900 dark:text-gray-300 dark:hover:text-white">
             <Avatar
-              image={BondData?.image_url}
-              alt={BondData?.name}
+              image={bondData?.image_url}
+              alt={bondData?.name}
               size="sm"
               className="text-ellipsis ltr:mr-3 rtl:ml-3 dark:border-gray-500"
             />
             <span className="overflow-hidden text-ellipsis">
-              {BondData?.name}
+              {bondData?.name}
             </span>
           </div>
         </div>
         <div className="relative block w-full pb-full">
           <Image
-            src={BondData?.image_url}
+            src={bondData?.image_url}
             // placeholder="blur"
             layout="fill"
             objectFit="cover"
@@ -50,15 +71,15 @@ export default function BondGrid({ project }: Props) {
         <div className="p-5">
           <div className="mt-1.5 flex">
             <div className="inline-flex items-center text-xs text-gray-600 dark:text-gray-400">
-              {BondData?.creator}
+              {bondData?.creator}
               <Verified className="ltr:ml-1 rtl:mr-1" />
             </div>
           </div>
           <div className="text-md mt-4 font-medium text-gray-900 dark:text-white">
-            Target Size: {Number(BondData?.target_funding_size) / 10 ** 8} APT
+            Target Size: {Number(bondData?.target_funding_size) / 10 ** 8} APT
           </div>
           <div className="text-md mt-4 font-medium text-gray-900 dark:text-white">
-            Already Raised: {Number(BondData?.funding?.value) / 10 ** 8} APT
+            Already Raised: {Number(bondData?.funding?.value) / 10 ** 8} APT
           </div>
         </div>
       </div>
